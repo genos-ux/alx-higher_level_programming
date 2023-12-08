c3718fe2f3df2a90065c9285d0592aa4b9b5213a
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2023-12-07 16:24:09 -0800

    add file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     # handle the case where the index (idx) is negative
     # check if the index is greater than or equal to the number of elements
     if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
+        return my_list
+    # replace the element at that index in my_list.
+    my_list[idx] = element
+    return my_list
